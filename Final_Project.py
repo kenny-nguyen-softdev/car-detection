740c49ebc63b0368299c7368c7fc5c22f69fa995
MiloHuy <91107200+MiloHuy@users.noreply.github.com>
2023-04-13 21:36:22 +0700

    update

--- Final_Project.py
+++ Final_Project.py
@@ -10,6 +10,9 @@ from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.clock import Clock
+import module
+import time
 
 LabelBase.register(
     name="font2", fn_regular="D:\Hk2_Nam3\Image_Processing\Final_Project\Font\\font2.ttf")
@@ -19,21 +22,22 @@ class WindowExit(Popup):
     pass
 
 
+class WindowLoadVideo(Popup):
+    pass
+
+
+class Content(FloatLayout):
+    load = ObjectProperty()
+    cancel = ObjectProperty()
+    pass
+
+
 class MainWindow(ScreenManager):
     pass
 
 
 class NotificationExitApp(FloatLayout):
-
-    def exit_app(self):
-        App.get_running_app().stop()
-        Window.close()
-    pass
-
-    def cancel_popup(self):
-        self.the_popup_exit = TheFirstScreen()
-        self.the_popup_exit.cancel_popup()
-    pass
+    cancel = ObjectProperty()
 
 
 class TheFirstScreen(Screen):
@@ -41,13 +45,12 @@ class TheFirstScreen(Screen):
 
     def open_popup(self):
         self.the_popup = WindowExit()
-        content_exit = NotificationExitApp()
+        content_exit = NotificationExitApp(cancel=self.cancel)
         self.the_popup.add_widget(content_exit)
         self.the_popup.open()
 
-    def cancel_popup(self):
-        print("cancel")
-        WindowExit().dismiss()
+    def cancel(self):
+        self.the_popup.dismiss()
     pass
 
 
@@ -56,13 +59,53 @@ class TheSecondScreen(Screen):
 
 
 class TheThirdScreen(Screen):
+    times = 0
+    end_time = 0
+    i = 0
+    file_path_video = StringProperty("No file chosen")
+    the_popup = ObjectProperty(None)
+    detect_car = module.ModuleOfCar()
 
     def __init__(self, **kwargs):
         super(TheThirdScreen, self).__init__(**kwargs)
 
+    def open_popup(self):
+        self.the_popup = WindowLoadVideo()
+        content = Content(load=self.load, cancel=self.cancel)
+        self.the_popup.add_widget(content)
+        self.the_popup.open()
+
+    def load(self, selection):
+        self.file_path_video = str(selection[0])
+        self.the_popup.dismiss()
+        try:
+            self.ids.video3.source = self.file_path_video
+            print("File name " + self.ids.video3.source)
+        except:
+            pass
+
+    def detect_object(self):
+        start_time = time.time()
+        self.detect_car.detection_car(self.file_path_video)
+        self.end_time = time.time() - start_time
+        self.ids.video3.source = "./Videos/video4.avi"
+        print(self.end_time)
+
+    def cancel(self):
+        self.the_popup.dismiss()
+
     def play_video(self):
         self.ids.video3.state = "play"
         print(self.ids.video3.state)
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_label, 0.5)
+
+    def update_label(self, *args):
+        self.times += 1
+        new_time = self.ids.time.text.replace(str(self.i), str(self.times))
+        self.ids.time.text = new_time
+        self.i += 1
 
     def pause_video(self):
         self.ids.video3.state = "pause"
@@ -78,6 +121,7 @@ kv = Builder.load_file("my.kv")
 
 
 class MyApp(App):
+
     def build(self):
         return kv
 
