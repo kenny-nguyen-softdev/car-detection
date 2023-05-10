71e81725ff454d828cc0dcdcce12ee1317d9ec73
MiloHuy <91107200+MiloHuy@users.noreply.github.com>
2023-05-10 21:32:11 +0700

    update

--- test.py
+++ test.py
@@ -1,27 +1,3 @@
-# from ultralytics import YOLO
-# import cv2 as cv
-# model = YOLO('model_bien_so.pt')
-# video_path = "./Videos/video70.mp4"
-# cap = cv.VideoCapture(video_path)
-# width_video = int(cap.get(3))
-# height_video = int(cap.get(4))
-# size_of_video = (width_video, height_video)
-# video_save = cv.VideoWriter(
-#     "./Videos/video4.avi", cv.VideoWriter_fourcc(*'MJPG'), 10, size_of_video)
-# while cap.isOpened():
-#     success, frame = cap.read()
-#     if success:
-#         results = model(frame)
-#         annotated_frame = results[0].plot()
-#         # video_save.write(annotated_frame)
-#         cv.imshow("Video", annotated_frame)
-#         if cv.waitKey(10) & 0xFF == ord('q'):
-#             break
-#     else:
-#         break
-# cap.release()
-# video_save.release()
-# cv.destroyAllWindows()
 import cv2 as cv
 from ultralytics import YOLO
 import numpy as np
@@ -42,7 +18,6 @@ while cap.isOpened():
     if success:
         results = model(frame)
         for r in results[0].boxes.data.tolist():
-            # print(r)
             x1, y1, x2, y2, score, class_id = r
             x1 = int(x1)
             y1 = int(y1)
@@ -53,11 +28,6 @@ while cap.isOpened():
             # Tu anh goc cat anh co chua bien so co toa do (x1:x2,y1:y2)
             new_img = image_car[y1:y2, x1:x2]
             cv.imwrite("new_image" + str(count) + ".png", new_img)
-        # annotated_frame = results[0].plot()
-        # video_save.write(annotated_frame)
-        # print(results[0].orig_img.shape)
-        # if cv.waitKey(10) & 0xFF == ord('q'):
-        #     break
     else:
         break
 end_time = time.time() - start_time
